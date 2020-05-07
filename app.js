9f1e62de72aa0b15daab899575d1bff1841710be
mrTran <anhvanluc1999@gmail.com>
2020-05-07 10:43:31 +0700

    Add app.use() method

--- app.js
+++ app.js
@@ -6,10 +6,16 @@ var port = 3000;
 app.set('view engine', 'pug');
 app.set('views', './views');
 
+
+
+
+app.use(express.static('public'));
+
+
 app.get('/', function(req, res) {
 	res.render('homepage');
 });
 
 app.listen(port, function() {
-	console.log('This app listening at http://localhost:${port}')
+	console.log(`This app listening at http://localhost:${port}`)
 });
