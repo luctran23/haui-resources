1565e1c8e8751dfc96e0ad467420352369a26b26
tranluc <anhvanluc1999@gmail.com>
2020-05-08 16:47:36 +0700

    Create /subjects page for the website

--- app.js
+++ app.js
@@ -6,9 +6,6 @@ var port = 3000;
 app.set('view engine', 'pug');
 app.set('views', './views');
 
-
-
-
 app.use(express.static('public'));
 
 
@@ -16,6 +13,15 @@ app.get('/', function(req, res) {
 	res.render('homepage');
 });
 
+app.get('/subjects', function(req, res) {
+	res.render('subjects/subjectPage', {
+		subjects: [
+			{id: 1, name: 'XML', description: 'Some example text. Some example text.', link: '#'},
+			{id: 2, name: 'JAVA', description: 'Some example text. Some example text.', link: '#'},
+			{id: 3, name: 'OOP using C++', description: 'Some example text. Some example text.', link: '#'}
+		]
+	});
+});
 app.listen(port, function() {
 	console.log(`This app listening at http://localhost:${port}`)
 });
