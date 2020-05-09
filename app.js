8c5bdae8d272b53b9f3fb7b2eaeb23a913fbe1fb
tranluc <anhvanluc1999@gmail.com>
2020-05-09 21:26:38 +0700

    Add search feature for the website

--- app.js
+++ app.js
@@ -8,18 +8,28 @@ app.set('views', './views');
 
 app.use(express.static('public'));
 
-
+var subjects = [
+	{id: 1, name: 'XML', description: 'Some example text. Some example text.', link: '#'},
+	{id: 2, name: 'JAVA', description: 'Some example text. Some example text.', link: '#'},
+	{id: 3, name: 'OOP using C++', description: 'Some example text. Some example text.', link: '#'}
+];
 app.get('/', function(req, res) {
 	res.render('homepage');
 });
 
 app.get('/subjects', function(req, res) {
 	res.render('subjects/subjectPage', {
-		subjects: [
-			{id: 1, name: 'XML', description: 'Some example text. Some example text.', link: '#'},
-			{id: 2, name: 'JAVA', description: 'Some example text. Some example text.', link: '#'},
-			{id: 3, name: 'OOP using C++', description: 'Some example text. Some example text.', link: '#'}
-		]
+		subjects: subjects
+	});
+});
+
+app.get('/subjects/search', function(req, res) {
+	var q = req.query.q;
+	var filteredSubjects = subjects.filter(function(subject) {
+		return subject.name.toLowerCase().indexOf(q.toLowerCase()) !== -1;
+	});
+	res.render('subjects/subjectPage', {
+		subjects: filteredSubjects
 	});
 });
 app.listen(port, function() {
