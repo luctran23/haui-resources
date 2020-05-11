b329f5977e9cebb9925a974170e12d04304159dd
tranluc <anhvanluc1999@gmail.com>
2020-05-11 15:00:23 +0700

    Save keyword in input field

--- public/events/script.js
+++ public/events/script.js
@@ -1,1 +1,14 @@
-alert('Welcome to Haui Learning');
+document.addEventListener('DOMContentLoaded', function(e) {
+    var input = document.getElementById('input');
+    input.value = sessionStorage.getItem('draft');
+    input.addEventListener('change', saveKeyWords);
+    function saveKeyWords() {
+    sessionStorage.setItem('draft', input.value);
+}
+});
+
+
+
+
+
+
